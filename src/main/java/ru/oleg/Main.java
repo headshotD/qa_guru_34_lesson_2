d493bdd1f2510f67b53585533c2d0982e223fd6e
VTB4101517 <namozov@dev.vtb.ru>
2025-02-27 21:31:00 +0300

    new file

---
+++ src/main/java/ru/oleg/Main.java
@@ -0,0 +1,19 @@
+package ru.oleg;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Close");
+        System.out.printf("This");
+        System.out.printf("54321");
+        System.out.printf("12345");
+
+        for (int i = 1; i <= 5; i++) {
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
