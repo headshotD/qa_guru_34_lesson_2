f4707913b3aada1ec39fe8cbaebf7546db2eac2a
Oleg <namozov@dev.vtb.ru>
2025-02-28 21:10:22 +0300

    fuck chromedriver

--- src/test/java/Demoqa.java
+++ src/test/java/Demoqa.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,27 +6,24 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Demoqa {
-    @AfterEach
-    void afterEach(){
-        System.out.println("### AfterEach");
-    }
-    @BeforeEach
-    void beforeEach(){
-        System.out.println("###  beforeEach");
-    }
+//    @AfterEach
+//    void afterEach() {
+//        System.out.println("### AfterEach");
+//    }
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        System.out.println("###  beforeEach");
+//    }
+
     @Test
-    void textBox() throws InterruptedException {
-        open ("https://demoqa.com/text-box");
-        $("[id='username']").setValue("Oleg Namozov");
-        wait(1000);
-        $("[id='userEmail']").setValue("Oleg@mail.ru");
-        wait(1000);
-        $("[id='currentAddress'").setValue("Moscow");
-        wait(1000);
-        $("[id='permanentAddress']").setValue("Moscow never sleeps");
-        wait(1000);
-        $("[id='submit']").click();
-        wait(1000);
+    void textBox() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Oleg Namozov");
+        $("#userEmail").setValue("Oleg@mail.ru");
+        $("#currentAddress").setValue("Moscow");
+        $("#permanentAddress").setValue("Moscow never sleeps");
+        $("#id='submit").click();
 
     }
 }
