8dcee5318a290a9889134342cfe1d92167b82e5e
VTB4101517 <namozov@dev.vtb.ru>
2025-02-28 19:41:06 +0300

    added JUnit 5

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,50 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.open;
+import org.junit.jupiter.api.*;
 
 public class Main {
+
+    int result;
+
+    @BeforeAll
+    static void beforeall() {
+        System.out.println("### BeforeAll\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###       beforeEach");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###       afterEach\n");
+        result = 0;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("###    AfterAll\n");
+    }
+
     @Test
-    void test(){
-        Assertions.assertTrue( 3 > 2);
+    void onetest() {
+        System.out.println("###          one test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void twotest() {
+        System.out.println("###          two test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void threetest() {
+        System.out.println("###          three test");
+        Assertions.assertTrue(result >= 1);
+    }
+
+    private int getResult() {
+        return 3;
     }
 }
